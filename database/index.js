667cdb15f8c4f86773542f8a25b60abc6088c961
JohnCharamonde <john.charamonde@gmail.com>
2020-01-27 17:43:04 -0500

    API get working to calculate prices

--- database/index.js
+++ database/index.js
@@ -8,4 +8,36 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-module.exports = connection;
+// get a hotel's record
+const getTargetHotelInfo = (id, callback) => {
+  connection.query(`SELECT * FROM hotels WHERE id=${id}`, (error, results) => {
+    if (error) callback(error, null);
+    callback(null,results);
+  })
+}
+
+// get date_premiums for each day of the proposed stay
+const getDatePremiumsForStay = (hotel, checkIn, checkOut, callback) => {
+  connection.query(`SELECT * FROM dates WHERE id>=${checkIn} AND id<=${checkOut}`, (error, results) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null,results);
+    }
+  })
+}
+
+// get all the site records
+const getSites = (hotel, averageDatePremium, callback) => {
+  connection.query(`SELECT * FROM sites`, (error, results) => {
+    if (error) callback(error, null);
+    callback(null,results);
+  })
+}
+
+module.exports = {
+  connection,
+  getTargetHotelInfo,
+  getDatePremiumsForStay,
+  getSites
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,57 @@
 const express = require('express');
+const connection = require('../database');
 const app = express();
 
-app.use(express.static('./public'));
+app.use('/id', express.static('./public'));
+app.use(express.json());
+
+app.get('/api/booking/:id/', (req, res) => {
+  // get the record for the hotel corresponding to the id passed through the url
+  connection.getTargetHotelInfo(req.params.id, (error, hotel) => {
+    if (error) {
+      console.error(error);
+    } else {
+      //check if proposed stay is longer than hotel's max_stay property - if so, update hotel with max_stay_exceeded property, set its value to true, and send as response.
+      if (req.body.checkOut - req.body.checkIn > hotel[0].max_stay) {
+        hotel[0].max_stay_exceeded = true;
+        res.status(200).send(hotel);
+      } else {
+        // otherwise, pass hotel to getDatePremiumsForStay to maintain access for further processing
+        connection.getDatePremiumsForStay(hotel, req.body.checkIn, req.body.checkOut, (error, datePremiums) => {
+          if (error) {
+            console.log(error);
+          } else {
+            // calculate average date premium for the stay
+            let averageDatePremium = datePremiums.reduce((acc, datePremium) => { return acc + datePremium.date_premium }, 0) / datePremiums.length;
+
+            // pass hotel and datePremium into getSites to maintain access for further processing
+            connection.getSites(hotel, averageDatePremium, (error, sites) => {
+              if (error) {
+                console.log(error)
+              } else {
+                // first element of pricingAndDealData is value at hotel's own deal property
+                let pricingAndDealData = [hotel[0].deal];
+
+                // calculate average price for the stay factoring in all variables EXCEPT each site's unique tweak factor
+                let adultExtraCost = req.body.adults > 2 ? (req.body.adults - 2) * hotel[0].adult_premium  * hotel[0].standard_rate : 0;
+                let childrenExtraCost = req.body.children > 1 ? (req.body.children - 1) * hotel[0].child_premium * hotel[0].standard_rate: 0;
+
+                let averagePrice = (hotel[0].standard_rate + adultExtraCost + childrenExtraCost) * averageDatePremium;
+
+                // apply each site's tweak factor to the averagePrice, make a site Object with name and price properties, push to pricingAndDealData
+                sites.forEach(site => {
+                  pricingAndDealData.push({ name: site.site_name, price: Math.ceil(averagePrice * site.tweak), logo: site.logo, incentive: site.incentive })
+                });
+                //send the formatted data
+                res.status(200).send(pricingAndDealData)
+              }
+            })
+          }
+        })
+      }
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
